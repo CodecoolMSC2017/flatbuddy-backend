939cf92769d09d2e209d6413c8016e5cf67ab8cb
peti120 <peti59550@gmail.com>
2018-07-09 13:46:40 +0200

    base data.sql added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users(
+	first_name, last_name, email, password, role, is_flatmate, age, gender, description, destination)
+	VALUES ('Test', 'Jozsi', 'a@a', 'asdasd', 'User', false, 23, 'Male', 'asaasdafsad', 'Budapest');
