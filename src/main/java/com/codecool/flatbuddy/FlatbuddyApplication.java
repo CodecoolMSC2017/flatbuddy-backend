7744e42aa9e23a6958f76cd34d77eeff04fc17c8
peti120 <peti59550@gmail.com>
2018-07-09 09:20:39 +0200

    skeleton created

---
+++ src/main/java/com/codecool/flatbuddy/FlatbuddyApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.flatbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatbuddyApplication.class, args);
+	}
+}
