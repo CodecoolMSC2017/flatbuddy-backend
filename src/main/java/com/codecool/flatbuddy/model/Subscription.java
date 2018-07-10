9e74cad2ac3c28a536ed01cfc5dd8932793e1f32
GaborMartin <gabormartin94@gmail.com>
2018-07-10 11:09:32 +0200

    Created Subscription entity, added fields, getters, setters

---
+++ src/main/java/com/codecool/flatbuddy/model/Subscription.java
@@ -0,0 +1,39 @@
+package com.codecool.flatbuddy.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name= "subscriptions")
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "ad_id")
+    private Integer adId;
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getAdId() {
+        return adId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setAdId(Integer adId) {
+        this.adId = adId;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
