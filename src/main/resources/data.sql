3280d4f9c6adf4bbe115e0b1f0df4c25fe7bbc39
GaborMartin <gabormartin94@gmail.com>
2018-07-10 10:16:07 +0200

    Added tester user and messages

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,17 @@
 INSERT INTO public.users(
 	first_name, last_name, email, password, role, is_flatmate, age, gender, description, destination)
 	VALUES ('Test', 'Jozsi', 'a@a', 'asdasd', 'User', false, 23, 'Male', 'asaasdafsad', 'Budapest');
+INSERT INTO public.users(
+	first_name, last_name, email, password, role, is_flatmate, age, gender, description, destination)
+	VALUES ('Test', 'Pista', 'b@b', 'asdasd', 'User', false, 23, 'Male', 'asaasdafsad', 'Budapest');
 INSERT INTO public.rent_ads(
 	user_id, country, state, city, zip_code, district, street, description, cost, size, type, is_furnitured, rooms_available, is_premium, published_date, expiration_date)
 	VALUES (1, 'Tadzsikisztán', 'Fityma', 'Anyam', 'It', 'van', 'galab', 'imadom', 5000, 10, 'posta', true, 5, true, '2001-10-10', '2022-10-10');
+
+INSERT INTO public.messages(
+	sender_id, receiver_id, content, date)
+	VALUES (1, 2, 'helo', '2018-07-10');
+
+INSERT INTO public.messages(
+	sender_id, receiver_id, content, date)
+	VALUES (1, 2, 'szia', '2018-07-09');
