c9e73d6eb7fd5247f7d5db291a55ab472dc91cf0
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-07-12 12:57:50 +0200

    RestMatchController implemented,methods from usercontroller placed over

---
+++ src/main/java/com/codecool/flatbuddy/controller/RestMatchController.java
@@ -0,0 +1,38 @@
+package com.codecool.flatbuddy.controller;
+
+import com.codecool.flatbuddy.model.Match;
+import com.codecool.flatbuddy.service.MatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class RestMatchController {
+
+    @Autowired
+    private MatchService matchService;
+
+    @PostMapping(path = "/user/match/send/{id}",
+            consumes = MediaType.ALL_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public void sendMatchRequest(@PathVariable("id")int id){
+        matchService.addToMatches(id);
+    }
+
+
+
+    @GetMapping(path = "/user/matches/{id}",
+            consumes = MediaType.ALL_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Match> getMatchesByUserAId(@PathVariable("id")int id){
+        return matchService.getByUserA(id);
+    }
+    @DeleteMapping(path = "/user/match/delete/{id}",
+            consumes = MediaType.ALL_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public void deleteMatches(@PathVariable("id")int id){
+        matchService.deleteMatch(id,1);//ide kell a bejelentkezett user!!!
+    }
+}
