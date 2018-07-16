836b6812aa851e379d70e8bc6de1dbd384ee0648
GaborMartin <gabormartin94@gmail.com>
2018-07-16 12:39:17 +0200

    Created basic DisabilityChecker, implemented checkObjectsIsEnabled method

---
+++ src/main/java/com/codecool/flatbuddy/util/DisabilityChecker.java
@@ -0,0 +1,36 @@
+package com.codecool.flatbuddy.util;
+
+import com.codecool.flatbuddy.model.Match;
+import com.codecool.flatbuddy.model.RentAd;
+import org.aspectj.weaver.ast.Instanceof;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisabilityChecker {
+    public static List<?> checkObjectsIsEnabled(List<?> list) {
+        if (list.isEmpty()) {
+            return list;
+        }
+        if (list.get(0) instanceof Match) {
+            List<Match> visibleMatches = new ArrayList<>();
+            for (int i = 0; i < list.size(); i++) {
+                Match element = (Match) list.get(i);
+                if (element.isEnabled()) {
+                    visibleMatches.add(element);
+                }
+            }
+            return visibleMatches;
+        } else if (list.get(0) instanceof RentAd) {
+            List<RentAd> visibleRentAds = new ArrayList<>();
+            for (int i = 0; i < list.size(); i++) {
+                RentAd element = (RentAd) list.get(i);
+                if (element.isEnabled()) {
+                    visibleRentAds.add(element);
+                }
+            }
+            return visibleRentAds;
+        }
+        return list;
+    }
+}
