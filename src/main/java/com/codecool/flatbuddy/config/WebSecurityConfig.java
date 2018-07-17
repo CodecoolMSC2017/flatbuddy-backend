5821053e830ace35c8e1346a6efe6d3977dbe2a7
GaborMartin <gabormartin94@gmail.com>
2018-07-17 11:44:24 +0200

    Updated config to db authentication instead of memoryauth

--- src/main/java/com/codecool/flatbuddy/config/WebSecurityConfig.java
+++ src/main/java/com/codecool/flatbuddy/config/WebSecurityConfig.java
@@ -3,13 +3,20 @@ package com.codecool.flatbuddy.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -20,14 +27,51 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+        http.httpBasic();
+
         http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
+
+
+        /* http.authorizeRequests()
                 .antMatchers("/user/**").permitAll()
                 .antMatchers("/**").hasRole("ADMIN");
 
         http.formLogin();
-        http.csrf().disable();
+        http.csrf().disable(); */
+
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    UserDetailsManager userDetailsManager(
+            @Autowired DataSource dataSource,
+            @Autowired JdbcTemplate jdbcTemplate,
+            @Autowired AuthenticationManager authenticationManager) {
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
+        userDetailsManager.setJdbcTemplate(jdbcTemplate);
+        userDetailsManager.setDataSource(dataSource);
+        // Using AuthenticationManager re-authenticates the session on password change.
+        userDetailsManager.setAuthenticationManager(authenticationManager);
+        return userDetailsManager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    /*
     // create two users, admin and user
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -41,5 +85,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    */
 }
 
