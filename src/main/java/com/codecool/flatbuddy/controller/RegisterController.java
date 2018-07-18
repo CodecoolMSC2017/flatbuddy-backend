5b5f842002552f28baed119f5a83882d3a9a9736
Bernáth Máté <bernath.mate.new@gmail.com>
2018-07-18 16:05:04 +0200

    The add() method is modified, it hasn't return value.

--- src/main/java/com/codecool/flatbuddy/controller/RegisterController.java
+++ src/main/java/com/codecool/flatbuddy/controller/RegisterController.java
@@ -16,10 +16,10 @@ public class RegisterController {
     private UserService userService;
 
     @PostMapping("/register")
-    public User add(@RequestBody Map<String, String> map) {
+    public void add(@RequestBody Map<String, String> map) {
         String newEmail = map.get("email");
         String newPassword = map.get("password");
         String newConfirmationPassword = map.get("confirmationPassword");
-        return userService.addNewUser(newEmail, newPassword, newConfirmationPassword);
+        userService.addNewUser(newEmail, newPassword, newConfirmationPassword);
     }
 }
