929eb8a45282b84ed07a91002a731eafc75afd96
GaborMartin <gabormartin94@gmail.com>
2018-07-27 13:45:57 +0200

    Implemented custom authentication entry point class to ignore basic auth pop up window

---
+++ src/main/java/com/codecool/flatbuddy/config/NoPopupBasicAuthenticationEntryPoint.java
@@ -0,0 +1,17 @@
+package com.codecool.flatbuddy.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class NoPopupBasicAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+    }
+}
