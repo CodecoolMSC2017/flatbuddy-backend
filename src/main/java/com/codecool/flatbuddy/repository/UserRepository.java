62e75248f338d96c36061b1a89d84aa638df4309
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-07-31 09:03:58 +0200

    added id check in the query of findPeople() method

--- src/main/java/com/codecool/flatbuddy/repository/UserRepository.java
+++ src/main/java/com/codecool/flatbuddy/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByEmail(String email);
-    @Query(value = "select * from users where id not in (Select users.id from users Join matches on users.id = matches.user_b where user_a =?1 or user_b =?1) and is_flatmate = true",nativeQuery = true)
+    @Query(value = "select * from users where id not in (Select users.id from users Join matches on users.id = matches.user_b where user_a =?1 or user_b =?1) and is_flatmate = true and id != ?1",nativeQuery = true)
     List<User> findPeople(int userId);
 
     List<User> findAllByisFlatmate(boolean flatmate);
