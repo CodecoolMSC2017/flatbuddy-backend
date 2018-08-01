208d53632a2537d205f9b6bec3d0e7f9ebd4a0dd
GaborMartin <gabormartin94@gmail.com>
2018-08-01 16:19:46 +0200

    Implemented getGoogleAuthenticatedUser, getUserByToken methods, optimized imports

--- src/main/java/com/codecool/flatbuddy/service/UserService.java
+++ src/main/java/com/codecool/flatbuddy/service/UserService.java
@@ -5,17 +5,35 @@ import com.codecool.flatbuddy.model.RentAd;
 import com.codecool.flatbuddy.model.User;
 import com.codecool.flatbuddy.repository.UserRepository;
 import com.codecool.flatbuddy.util.DisabilityChecker;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public final class UserService {
+
+    HttpTransport transport = new NetHttpTransport();
+    JsonFactory jsonFactory = new JacksonFactory();
 
     @Autowired
     private UserRepository repository;
@@ -109,6 +127,15 @@ public final class UserService {
         return repository.findByEmail(email);
     }
 
+    public User getGoogleAuthenticatedUser(String email) {
+        if (!isEmailExists(email)) {
+            userManager.createUser(new org.springframework.security.core.userdetails.User(
+                    email, "",
+                    AuthorityUtils.createAuthorityList("USER_ROLE")));
+        }
+        return getUserByEmail(email);
+    }
+
     private boolean isEmailExists(String email) {
         User user = repository.findByEmail(email);
 
@@ -117,5 +144,40 @@ public final class UserService {
         }
 
         return true;
+    }
+
+    public User getUserByToken(String idToken) throws GeneralSecurityException, IOException {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory).setAudience(Collections.singletonList("87138340487-dna6m556e14folussr8dqq5pn0k3em07.apps.googleusercontent.com")).build();
+
+        GoogleIdToken token = verifier.verify(idToken);
+
+        if (token != null) {
+            GoogleIdToken.Payload payload = token.getPayload();
+            String userId = payload.getSubject();
+
+            String email = payload.getEmail();
+            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+
+            User user = getGoogleAuthenticatedUser(email);
+
+            List<GrantedAuthority> roles = user.getAuthorities()
+                    .stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+
+            Authentication auth = new UsernamePasswordAuthenticationToken(user.getEmail(), null, roles);
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+            return user;
+        } else {
+            System.out.println("Invalid ID token.");
+            return null;
+        }
     }
 }
