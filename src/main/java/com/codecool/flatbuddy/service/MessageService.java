90bdaa7d088edbf5398a9f8f16bb00668239c288
GaborMartin <gabormartin94@gmail.com>
2018-08-14 10:38:04 +0200

    Implemented checkIsEnabled method, refactored methods.

--- src/main/java/com/codecool/flatbuddy/service/MessageService.java
+++ src/main/java/com/codecool/flatbuddy/service/MessageService.java
@@ -4,13 +4,13 @@ import com.codecool.flatbuddy.exception.*;
 import com.codecool.flatbuddy.model.Message;
 import com.codecool.flatbuddy.model.User;
 import com.codecool.flatbuddy.repository.MessageRepository;
-import com.codecool.flatbuddy.util.DisabilityChecker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -27,7 +27,7 @@ public class MessageService {
     public List<Message> getReceivedMessages() throws NoMessagesException {
         User loggedInUser = userService.getUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
         List<Message> allMessages = msgRepository.findAllByReceiverId(loggedInUser.getId());
-        List<Message> visibleMessages = (List<Message>) DisabilityChecker.checkObjectsIsEnabled(allMessages);
+        List<Message> visibleMessages = checkIsEnabled(allMessages);
 
         if (visibleMessages.isEmpty()) {
             throw new NoMessagesException();
@@ -39,7 +39,7 @@ public class MessageService {
     public List<Message> getSentMessages() throws NoMessagesException {
         User loggedInUser = userService.getUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
         List<Message> allMessages = msgRepository.findAllBySenderId(loggedInUser.getId());
-        List<Message> visibleMessages = (List<Message>) DisabilityChecker.checkObjectsIsEnabled(allMessages);
+        List<Message> visibleMessages = checkIsEnabled(allMessages);
 
         if (visibleMessages.isEmpty()) {
             throw new NoMessagesException();
@@ -55,7 +55,11 @@ public class MessageService {
         try {
             Message message = msgRepository.findById(Integer.valueOf(messageId)).get();
             if (message.getReceiverId() == loggedInUser.getId() || message.getSenderId() == loggedInUser.getId()) {
-                if (message.isEnabled()) {
+                if (message.isEnabledToReceiver() && message.isEnabledToSender()) {
+                    return message;
+                } else if (loggedInUser.getId() == message.getSenderId() && message.isEnabledToSender()) {
+                    return message;
+                } else if (loggedInUser.getId() == message.getReceiverId() && message.isEnabledToReceiver()) {
                     return message;
                 } else {
                     throw new DeletedMessageException();
@@ -70,7 +74,7 @@ public class MessageService {
 
 
 
-    public void sendMessage(int receiverId, String subject, String content) throws InvalidSubjectException, InvalidContentException {
+    public void sendMessage(String receiverId, String subject, String content) throws InvalidSubjectException, InvalidContentException, InvalidMessageSendingException {
 
         if (subject == null || subject.equals("")) {
             throw new InvalidSubjectException();
@@ -80,14 +84,21 @@ public class MessageService {
             throw new InvalidContentException();
         }
 
+        try {
+            User user = userService.getUserById(Integer.valueOf(receiverId));
+        } catch (NumberFormatException | NoSuchElementException ex) {
+            throw new InvalidMessageSendingException();
+        }
+
         User loggedInUser = userService.getUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
 
         Message newMessage = new Message();
         newMessage.setSenderId(loggedInUser.getId());
-        newMessage.setReceiverId(receiverId);
+        newMessage.setReceiverId(Integer.valueOf(receiverId));
         newMessage.setContent(content);
         newMessage.setSubject(subject);
-        newMessage.setEnabled(true);
+        newMessage.setEnabledToSender(true);
+        newMessage.setEnabledToReceiver(true);
 
         Date currentDate = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
@@ -102,13 +113,30 @@ public class MessageService {
         User loggedInUser = userService.getUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
         try {
             Message msg = msgRepository.findById(Integer.valueOf(messageId)).get();
-            if (msg.getSenderId() != loggedInUser.getId()) {
+            if (loggedInUser.getId() == msg.getReceiverId()) {
+                msg.setEnabledToReceiver(false);
+            } else if (loggedInUser.getId() == msg.getSenderId()) {
+                msg.setEnabledToSender(false);
+            } else {
                 throw new InvalidMessageAccessException();
             }
-            msg.setEnabled(false);
             msgRepository.save(msg);
         } catch (EmptyResultDataAccessException | NumberFormatException | NoSuchElementException ex) {
             throw new InvalidMessageIdException();
         }
+    }
+
+    private List<Message> checkIsEnabled(List<Message> messages) {
+        User loggedInUser = userService.getUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+        List<Message> visibleMessages = new ArrayList<Message>();
+        for (int i = 0; i < messages.size() ; i++) {
+            Message msg = messages.get(i);
+            if (msg.isEnabledToSender() && msg.getSenderId() == loggedInUser.getId()) {
+                visibleMessages.add(msg);
+            } else if (msg.isEnabledToReceiver() && msg.getReceiverId() == loggedInUser.getId()) {
+                visibleMessages.add(msg);
+            }
+        }
+        return visibleMessages;
     }
 }
