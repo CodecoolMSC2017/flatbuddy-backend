7e2aa00b3944cfc964a77f94581127f57880344d
peti120 <peti59550@gmail.com>
2018-09-03 10:50:06 +0200

    Repository fix

--- src/main/java/com/codecool/flatbuddy/repository/AdCommentRepository.java
+++ src/main/java/com/codecool/flatbuddy/repository/AdCommentRepository.java
@@ -1,11 +1,12 @@
 package com.codecool.flatbuddy.repository;
 
 import com.codecool.flatbuddy.model.AdComment;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface AdCommentRepository {
+public interface AdCommentRepository extends CrudRepository<AdComment,Integer> {
     List<AdComment> findAllByAdId(Integer adid);
 }
