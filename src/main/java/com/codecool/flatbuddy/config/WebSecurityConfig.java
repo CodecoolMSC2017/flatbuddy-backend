70155e1f1fdabd5d004267d7d6c4bef6daaef2cd
peti120 <peti59550@gmail.com>
2018-09-04 11:10:47 +0200

    /media endpoint accessable for guests

--- src/main/java/com/codecool/flatbuddy/config/WebSecurityConfig.java
+++ src/main/java/com/codecool/flatbuddy/config/WebSecurityConfig.java
@@ -35,6 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/register").permitAll()
                 .antMatchers("/advertisements/**").permitAll()
                 .antMatchers("/advertisement/**").permitAll()
+                .antMatchers("/media/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
